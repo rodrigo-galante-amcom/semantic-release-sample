bbedde1984fac9e76ca02fa28f5d43bb9cda7e55
Rodrigo Augusto Galante <rodrigo.galante@amcom.com.br>
2020-05-14 19:08:11 -0300

    feat: teste

--- SemanticReleaseSample/Program.cs
+++ SemanticReleaseSample/Program.cs
@@ -6,7 +6,7 @@ namespace SemanticReleaseSample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Semantic Release versioning!");
         }
     }
 }
